091d050ca65eb08bedfb796e9e4d4b8d393b8751
NeverStepD7 <NeverStepD7@gmail.com>
2026-02-09 12:26:16 -0600

    Update: QOL
    Added Occulucion cooling
    Addedd New sounds for the Jukebox
    Added a game tittle sccreen logo
    Added credits and coffee dog logo

--- Assets/Scripts/Jukebox.cs
+++ Assets/Scripts/Jukebox.cs
@@ -6,17 +6,38 @@ public class Jukebox : MonoBehaviour
     public AudioClip[] audioClips;
     public float minWaitTime;
     public float maxWaitTime;
+
+    public float minPitch;
+    public float maxPitch;
+
+    private int _trackBeingPlayed = 0;
     float delay;
 	private void Update()
 	{
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            _trackBeingPlayed = _trackBeingPlayed == audioClips.Length - 1? 0: _trackBeingPlayed + 1;
+            goto playMusic;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+            goto randomMusic;
+
         if (mAudio.isPlaying)
             return;
         delay -= Time.deltaTime;
         if (delay > 0)
             return;
+
+        randomMusic:
+		_trackBeingPlayed = Random.Range(0, audioClips.Length);
+        
+
+        playMusic:
         RandomizeDelay();
-		var randomMusic = Random.Range(0, audioClips.Length);
-        mAudio.clip = audioClips[randomMusic];
+
+        var randomPitch = Random.Range(minPitch, maxPitch);
+        mAudio.pitch = randomPitch;
+        mAudio.clip = audioClips[_trackBeingPlayed];
         mAudio.Play();
 	}
     public void RandomizeDelay()
